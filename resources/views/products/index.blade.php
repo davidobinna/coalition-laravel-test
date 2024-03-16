1577587a9b2d68343c28df59ba17205b5fe3622e
david <davidobinna579@gmail.com>
2024-03-16 15:10:35 +0100

    views-setup3

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
  @section('content')
+      <form action="">
+         @csrf
 
+         <div>
+            <label for="name">Product Name </label>
+         </div>
+         
 
-   <div>
-    <h1>Sample page</h1>
-   </div>
+      </form>
  @endsection
