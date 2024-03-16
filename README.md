2bae87105fe7be7d03f8c6bc664099ff8ad5844f
david <davidobinna579@gmail.com>
2024-03-16 15:06:52 +0100

    views-setup2

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ php artisan ui bootstrap --auth
 npm install
 npm run dev (pls run twice)
 
+I have Also included the coalation test for 
+
 
 
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,12 +11,12 @@ class ProductController extends Controller
     {
        $products   = Product::orderBy('created_at', 'desc' )->get();
        $theTotalProductValue  = $products->sum('quantity * price');
-       return view('', compact('products', 'theTotalProductValue'));
+       return view('products.index', compact('products', 'theTotalProductValue'));
     }
 
     public function store(Request $request)
     {
-        
+
     }
 
     public function show($id)
