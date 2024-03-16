d542d3a8b73d34bc15466a420bf8e55d8166a5f1
david <davidobinna579@gmail.com>
2024-03-16 14:46:17 +0100

    controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+       $products   = Product::orderBy('created_at', 'desc' )->get();
+       $theTotalProductValue  = $products->sum('quantity * price');
+       return view('', compact('products', 'theTotalProductValue'));
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
 }
